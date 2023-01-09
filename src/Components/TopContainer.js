b4ac0e3202b2d353fc8d264d03c5860c228b5192
SoWhiteBread <eldon.a.carpenter@gmail.com>
2023-01-09 23:08:20 +0000

    Update TopContainer.js and styles.css

--- src/Components/TopContainer.js
+++ src/Components/TopContainer.js
@@ -4,23 +4,11 @@ export const TopContainer = () => {
   return (
     <div>
       <div className="header">
-        <div className="wrapperLeft">
-          <img src="white.png" alt="Skill Dar Logo" className="logo" />
-        </div>
         <div className="menu">
-          <div className="explore">EXPLORE CREATORS</div>
-          <div className="myProjects">MY PROJECTS</div>
+          <div className="explore">THE CAMPAIGN</div>
+          <div className="myProjects">THE GANGS</div>
+          <div className="myProjects">THE BATTLES</div>
         </div>
-        <div className="wrapperRight">
-          <img src="ProfilePic.png" alt="Profile" className="profile" />
-        </div>
-      </div>
-      <div className="splash">
-        <div className="splashTextWrapper">
-          <div className="splashH1">Creative motion projects</div>
-          <div className="splashH2">Discover client work to pitch on.</div>
-        </div>
-        <div className="credit">Video by Harold Miles</div>
       </div>
     </div>
   );
