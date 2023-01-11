a1fd1a04f92f7cfe6d2becfafba33a65d02983be
SoWhiteBread <eldon.a.carpenter@gmail.com>
2023-01-11 03:48:36 +0000

    Update 2 files

---
+++ src/Components/Campaign.js
@@ -0,0 +1,20 @@
+import { React } from "react";
+
+export const Campaign ({ campaignData }) => {
+    return (
+        <div>
+            <div>
+            <p>A long forgotten underhive dome built to surround a colossal tomb complex, Beladon's Rest is a
+              sprawling labyrinth of rusted and decaying pre-fabricated shelters, derelict manufactories, and ancient cyclopean machinery.
+              Recently rediscovered by enterprising Domerunners, Beladon's Rest is newly lit by muzzles flares and lasfire as the gangs of the 
+              Underhive seek to carve out new territory from the ruins.</p>
+            <p>As battle rages throughout the reclaimed hab-blocks and around industrial monoliths, rumours spread that Beladon's Rest may
+              not be as abandoned as it seems</p>
+            <p>The Devastation of Beladon's Rest is based around a Dominion campaign for Necromunda Underhive with a focus on narrative and role-playing.</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
